4e9dc6c7f747b7ad6f15a5792661c31cdd768f4a
HanaokaY <okayu5757@ezweb.ne.jp>
2025-04-03 21:26:22 +0900

    Apply .gitignore settings

---
+++ blog/tests/test_models.py
@@ -0,0 +1,8 @@
+import pytest
+from blog.models import Post
+
+@pytest.mark.django_db
+def test_create_post():
+    post = Post.objects.create(title="Test Title", content="Test Content")
+    assert post.title == "Test Title"
+    assert post.content == "Test Content"
---
+++ blog/tests/tests_views.py
@@ -0,0 +1,10 @@
+import pytest
+from django.urls import reverse
+from blog.models import Post
+
+@pytest.mark.django_db
+def test_post_list_view(client):
+    Post.objects.create(title="Test Post", content="Test Content")
+    response = client.get(reverse("post_list"))
+    assert response.status_code == 200
+    assert "Test Post" in response.content.decode()
